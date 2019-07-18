3fa8d1c4f45e9b08351507b7d25a3db11317e4cf
Svetozar <velesvolkbeliy@gmail.com>
2019-07-18 20:25:37 +0300

    test_suit complete

--- test_timing.py
+++ test_timing.py
@@ -1,5 +1,5 @@
 from time import process_time
-from itertools import starmap
+from itertools import starmap, combinations_with_replacement as comb
 from Calk_Svetozar.Svetozar_05_dict import *
 
 
@@ -12,12 +12,13 @@ def calc_time(value1, value2, notation, operation):
 
 
 length_list = [1, 5, 10, 20, 50]  # список количества разрядов чисел для тестов
-
-test_suit_add = [
-    [(i, j, '2', '+') for i in ['1'*k for k in length_list] for j in ['1'*k for k in length_list]],
-    [(i, j, '8', '+') for i in ['7'*k for k in length_list] for j in ['7'*k for k in length_list]],
-    [(i, j, '10', '+') for i in ['9'*k for k in length_list] for j in ['9'*k for k in length_list]],
-    [(i, j, '16', '+') for i in ['F'*k for k in length_list] for j in ['F'*k for k in length_list]]
+num_sys_list = [['2', '1'], ['8', '7'], ['10', '9'], ['16', 'F']]
+operations = ['+', '-', '*', '//']
+test_suit = [
+    [(i, j, num_sys, operation)
+     for num_sys, num in num_sys_list
+     for i, j in comb([num*k for k in length_list], 2)]
+    for operation in operations
 ]
 
-addition_time = [list(starmap(calc_time, num_sys)) for num_sys in test_suit_add]
+# addition_time = [list(starmap(calc_time, num_sys)) for num_sys in test_suit_add]
