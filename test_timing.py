2fd73f071488c027169c322ab5969bddd19d1dc8
Svetozar <velesvolkbeliy@gmail.com>
2019-07-18 17:45:29 +0300

    working addition_time module

--- test_timing.py
+++ test_timing.py
@@ -1,14 +1,23 @@
 from time import process_time
+from itertools import starmap
 from Calk_Svetozar.Svetozar_05_dict import *
 
 
-def calc_time(func):
+def calc_time(value1, value2, notation, operation):
     time = process_time()
     for i in range(100):
-        func()
+        to_calc(value1, value2, notation, operation)
     time = (process_time() - time) / 100
     return time
 
 
-to_calc = calc_time(to_calc)
+length_list = [1, 5, 10, 20, 50]  # список количества разрядов чисел для тестов
 
+test_suit_add = [
+    [(i, j, '2', '+') for i in ['1'*k for k in length_list] for j in ['1'*k for k in length_list]],
+    [(i, j, '8', '+') for i in ['7'*k for k in length_list] for j in ['7'*k for k in length_list]],
+    [(i, j, '10', '+') for i in ['9'*k for k in length_list] for j in ['9'*k for k in length_list]],
+    [(i, j, '16', '+') for i in ['F'*k for k in length_list] for j in ['F'*k for k in length_list]]
+]
+
+addition_time = [list(starmap(calc_time, num_sys)) for num_sys in test_suit_add]
