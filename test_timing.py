cfd73d927a7924fc919f1c32d7a103f1b751bb42
Svetozar <velesvolkbeliy@gmail.com>
2019-07-19 15:53:21 +0300

    fixed bugs and changed time format to nanoseconds

--- test_timing.py
+++ test_timing.py
@@ -1,14 +1,14 @@
-from time import process_time
-from itertools import starmap, combinations_with_replacement as comb
+from time import perf_counter_ns
+from itertools import starmap
 from Calk_Svetozar.Svetozar_05_dict import *
 
 
 def maker(function):
     def calc_time(value1, value2, notation, operation):
-        time = process_time()
+        start = perf_counter_ns()
         function(value1, value2, notation, operation)
-        time = (process_time() - time)
-        return time
+        end = perf_counter_ns()
+        return end - start
     return calc_time
 
 
@@ -16,22 +16,28 @@ length_list = [1, 5, 10, 20, 50]  # список количества разря
 num_sys_list = [['2', '1'], ['8', '7'], ['10', '9'], ['16', 'F']]
 operations = ['+', '-', '*', '//']
 test_suit = [
-    [(i, j, num_sys, operation)
-     for num_sys, num in num_sys_list
-     for i, j in comb([num*k for k in length_list], 2)]
+    [
+        [
+            (i, j, num_sys, operation)
+            for i in [num*k for k in length_list]
+            for j in [num*k for k in length_list]
+        ]
+        for num_sys, num in num_sys_list
+    ]
     for operation in operations
 ]
-
 
 test_functions = [to_calc]
 for func in test_functions:
     timer = maker(func)
-    operations_times = [list(starmap(timer, num_sys)) for num_sys in test_suit]
-    print("Тестируемая функция: {0}".format(str(func)))
+    operations_times = [[list(starmap(timer, num_sys)) for num_sys in operator] for operator in test_suit]
+    print("Функция: {}".format(str(func.__name__)))
     for i in range(len(operations)):
-        print("\tТестируемая операция: {0}".format(operations[i]))
+        print("Операция: {}".format(operations[i]))
         for j in range(len(num_sys_list)):
-            print("\t\t{0[0]}: {1}".format(num_sys_list[j],
-                                           operations_times[i][j*len(operations_times)//len(operations):
-                                                               (j+1)*len(operations_times)//len(operations)]))
-    print("Суммарнове время прохождения теста: {0}".format(sum(operations_times)))
+            print("Система счисления: {}".format(num_sys_list[j][0]))
+            print("\t"+"\t\t\t\t".join(map(str, length_list)))
+            for k in range(len(length_list)):
+                space = (3 - len(str(operations_times[i][j][k*len(length_list)]))//8) * "\t"
+                print(str(length_list[k])+"\t" +
+                      space.join(map(str, operations_times[i][j][k*len(length_list):(k+1)*len(length_list)])))
