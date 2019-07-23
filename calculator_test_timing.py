59066e903808ee7455fa3a852c76e34acd90623c
Svetozar <velesvolkbeliy@gmail.com>
2019-07-23 21:56:47 +0300

    results test_timing write into .tsv format

--- calculator_test_timing.py
+++ calculator_test_timing.py
@@ -28,17 +28,19 @@ test_suit = [
     for operation in operations
 ]
 
-test_functions = [calc_svetozar, calc_marat]
-for func in test_functions:
-    timer = maker(func)
-    operations_times = [[list(starmap(timer, num_sys)) for num_sys in operator] for operator in test_suit]
-    print("Функция: {}".format(str(func.__name__)))
-    for i in range(len(operations)):
-        print("Операция: {}".format(operations[i]))
-        for j in range(len(num_sys_list)):
-            print("Система счисления: {}".format(num_sys_list[j][0]))
-            print("\t"+"\t\t\t\t".join(map(str, length_list)))
-            for k in range(len(length_list)):
-                space = (3 - len(str(operations_times[i][j][k*len(length_list)]))//8) * "\t"
-                print(str(length_list[k])+"\t" +
-                      space.join(map(str, operations_times[i][j][k*len(length_list):(k+1)*len(length_list)])))
+with open("calculator_test_timing_results.tsv", "w") as file_out:
+    test_functions = [calc_svetozar, calc_marat]
+    for func in test_functions:
+        timer = maker(func)
+        operations_times = [[list(starmap(timer, num_sys)) for num_sys in operator] for operator in test_suit]
+        file_out.write("Функция\t{}\n".format(str(func.__name__)))
+        for i in range(len(operations)):
+            file_out.write("Операция\t{}\n".format(operations[i]))
+            for j in range(len(num_sys_list)):
+                file_out.write("Система счисления\t{}\n".format(num_sys_list[j][0]))
+                file_out.write("Количество цифр\t" + "\t".join(map(str, length_list)) + "\n")
+                for k in range(len(length_list)):
+                    file_out.write(
+                        str(length_list[k]) + "\t" +
+                        "\t".join(map(str, operations_times[i][j][k*len(length_list):(k+1)*len(length_list)])) + "\n"
+                    )
